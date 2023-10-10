b3efe05143d43d1e11f1c31d776ee93f57afc4db
Andrej-Juric <andrej.juric991@gmail.com>
2023-10-10 15:51:10 +0200

    tooltip

---
+++ script.js
@@ -0,0 +1,38 @@
+class Tooltip {
+  constructor(selector, content, options) {
+    this.selector = selector;
+    this.content = content;
+    this.options = options;
+  }
+  static attach(selector, content, options) {
+    const elements = document.querySelectorAll(selector);
+
+    elements.forEach((element) => {
+      element.addEventListener("mouseenter", () => {
+        const tooltip = document.createElement("div");
+        tooltip.className = "tooltip-text";
+        tooltip.textContent = content;
+
+        tooltip.classList.add(`tooltip-${options.position || "top"}`);
+        tooltip.classList.add(`tooltip-${options.theme || "info"}`);
+        tooltip.classList.add(`animation-${options.animation || ""}`);
+
+        element.appendChild(tooltip);
+      });
+
+      element.addEventListener("mouseleave", () => {
+        const tooltip = element.querySelector(".tooltip-text");
+        if (tooltip) {
+          tooltip.remove();
+        }
+      });
+    });
+  }
+}
+
+Tooltip.attach(".tooltip", "Front Tribe!", {
+  position: "top", // top, right, bottom, left
+  theme: "info", // info, warning, error, success,
+  animation: "fade", // fade, slide, expand
+  delay: 500,
+});
